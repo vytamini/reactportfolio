e79b7196f1c91631c9dd0ed94fba3ca2b4bbf81b
vytamini <vytamini1@gmail.com>
2018-11-24 13:04:37 +0100

    Updates

---
+++ precache-manifest.bf5fad6ac82f489d235d7025bf7018c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6a8752e86483642952bcbdde939c1b",
+    "url": "/reactportfolio/static/media/karneval.0a6a8752.png"
+  },
+  {
+    "revision": "df9e7c7d3f9c01ae9f46a5a3d5b9c075",
+    "url": "/reactportfolio/static/media/chokolade.df9e7c7d.png"
+  },
+  {
+    "revision": "8aed36fa3f8b74a9c6bd09136c8633f3",
+    "url": "/reactportfolio/static/media/apel.8aed36fa.png"
+  },
+  {
+    "revision": "f3444e3a511a2aa26237",
+    "url": "/reactportfolio/static/js/runtime~main.f3444e3a.js"
+  },
+  {
+    "revision": "787a7f6f22c62c049289",
+    "url": "/reactportfolio/static/js/main.787a7f6f.chunk.js"
+  },
+  {
+    "revision": "80bb9f8bbf9d5650376e",
+    "url": "/reactportfolio/static/js/1.80bb9f8b.chunk.js"
+  },
+  {
+    "revision": "787a7f6f22c62c049289",
+    "url": "/reactportfolio/static/css/main.1ddf7635.chunk.css"
+  },
+  {
+    "revision": "bc1f166147d28762733b13c211210958",
+    "url": "/reactportfolio/index.html"
+  }
+];
